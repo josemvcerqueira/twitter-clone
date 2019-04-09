d2208a23445516153b5def3937c38eda7614ad85
Jose Cerqueira <jose@origensmedia.com>
2019-04-09 18:14:57 +0100

    feat: connected App to the store and fetched the initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
 	render() {
 		return <div className="App">Hello World</div>;
 	}
 }
 
-export default App;
+export default connect()(App);
