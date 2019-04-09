9440c45b10dbf189fe07c06eaf18cfd87870c0bd
Jose Cerqueira <jose@origensmedia.com>
2019-04-09 23:02:08 +0100

    added the functionality to like and dislike a tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,15 +6,25 @@ import {
 	TiHeartOutline,
 	TiHeartFullOutline
 } from "react-icons/ti/index";
+import { handleToggleTweet } from "../actions/tweets";
 
 class Tweet extends Component {
 	toParent = (event, id) => {
 		event.preventDefault();
-		// todo: Redirect to parent Tweet.
 	};
 
 	handleLike = event => {
 		event.preventDefault();
+
+		const { dispatch, tweet, authedUser } = this.props;
+
+		dispatch(
+			handleToggleTweet({
+				id: tweet.id,
+				hasLiked: tweet.hasLiked,
+				authedUser
+			})
+		);
 
 		// todo: handle like tweet
 	};
